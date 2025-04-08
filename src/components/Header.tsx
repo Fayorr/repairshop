c775af7bc150fe44f9124e59f56aeb4570cd0272
fayorr <fayokunmiosho@gmail.com>
2025-04-08 06:37:02 +0100

    module 1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,41 @@
-
-import {HomeIcon, File, UsersRound } from "lucide-react";
-import Link from "next/link";
-import { NavButton } from "./NavButton";
+import { HomeIcon, File, UsersRound } from 'lucide-react';
+import Link from 'next/link';
+import { NavButton } from './NavButton';
 
 export function Header() {
-    return (
-			<header className='animate-slide bg-black h-12 p-2 border-b sticky top-0 z-10'>
-				<div className='flex justify-between items-center h-8 w-full'>
-                <div className='flex items-center gap-2'>
-                    <NavButton href='/home' label="home" icon={HomeIcon} />
-                    </div>
-					<div className='flex items-center'></div>
+	return (
+		<header className='animate-slide bg-white h-12 p-2 border-b sticky top-0 z-10'>
+			<div className='flex justify-between items-center h-8 w-full'>
+				<div className='flex items-center gap-2'>
+					<NavButton
+						href='/home'
+						label='home'
+						icon={HomeIcon}
+					/>
+					<Link
+						href='/home'
+						className='flex justify-center items-center gap-2 ml-0'
+						title='Home'
+					>
+						<h1 className='hidden sm:block text-xl font-bold m-0 mt-1'>
+							Computer Repair Shop
+						</h1>
+					</Link>
 				</div>
-			</header>
-		);
+
+				<div className='flex items-center'>
+					<NavButton
+						href='/tickets'
+						label='Tickets'
+						icon={File}
+					/>
+					<NavButton
+						href='/customers'
+						label='Customers'
+						icon={UsersRound}
+					/>
+				</div>
+			</div>
+		</header>
+	);
 }
