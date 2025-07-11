5ccb189e6cf8862a57245b34ef1f84dfdb988464
fayorr <fayokunmiosho@gmail.com>
2025-07-11 03:42:27 -0400

    fixed route

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -2,24 +2,18 @@ import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware';
 import { NextRequest } from 'next/server';
 
 export default withAuth(
-	async function middleware(request: NextRequest) {
-		console.log(request);
-		// Add your middleware logic here
-	},
-	{
-		isReturnToCurrentPage: true,
-	}
+  async function middleware(request: NextRequest) {
+    console.log(request);
+    // Add your middleware logic here
+  },
+  {
+    isReturnToCurrentPage: true,
+  },
 );
 
 export const config = {
-	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - api (API routes)
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico, robots.txt, images, login d
-		 */
-		'/((?!api|_next/static|_next/image|favicon.ico|robots.txt|images|login).*)',
-	],
+  matcher: [
+    // Exclude homepage (/) and other paths
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|images|login|^$).*)',
+  ],
 };
