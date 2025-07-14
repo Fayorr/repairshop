987a643465758235d452e1a6a66b6d0e585d0169
fayorr <fayokunmiosho@gmail.com>
2025-07-14 17:15:20 -0400

    added react-hook-form and ShadCnUi

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware';
+import { NextRequest } from 'next/server';
+
+export default withAuth(
+  async function middleware(request: NextRequest) {
+    console.log(request);
+  },
+  {
+    isReturnToCurrentPage: true,
+  },
+);
+
+export const config = {
+  matcher: ['/home/:path*', '/tickets/:path*', '/customers/:path*'],
+};
